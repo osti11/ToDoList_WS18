7b20fca9e2b031c5044d408e955f3ca98c90aaff
Jannik Ostermayer <inf2745@hs-worms.de>
2018-08-01 19:27:24 +0200

    create DbHelper class to manage the SQL-database on phone

---
+++ app/src/main/java/com/ema/jannik/todolist/DbHelper.kt
@@ -0,0 +1,29 @@
+package com.ema.jannik.todolist
+
+import android.content.Context
+import android.database.DatabaseErrorHandler
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+/**
+ * Database helper class extends SQLiteOpenHelper.
+ * This class create and manage the SQL database on the phone.
+ * Created by Jannik on 8/1/2018.
+ */
+
+public class DbHelper(context: Context?, name: String?, factory: SQLiteDatabase.CursorFactory?,
+                      version: Int) : SQLiteOpenHelper(context, name, factory, version) {
+
+
+    /**
+     *
+     */
+    override fun onCreate(db: SQLiteDatabase?) {
+
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
